51d85cc5b57003cb1d47c5d033c3d045a30f0532
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-12-10 17:35:07 +0000

    Updated URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var app_axios = axios.create({
-    baseURL: 'http://absolution.hl.edi.teebrb.net:4434'
-});
+    baseURL: 'https://imaginary.botbot.on.teebrb.net'
+})
 
 export default app_axios;
